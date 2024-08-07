718bf1a9b1eb6b16f59d92e80ee625ed89b05281
Johannes Wienke <languitar@semipol.de>
2024-08-07 20:57:16 +0200

    Generate REST resources and models from an OpenAPI spec
    This commit adds an example OpenAPI specification (the default Pets API)
    to the project and sets up the Maven OpenAPI generator plugin to
    generate API interfaces and models that are used for implementing the
    API inside the Quarkus project.
    
    The general configuration tweaks are documented inline. A few quirks of
    this setup will be fixed in the following commits.

---
+++ src/main/kotlin/de/semipol/adapters/rest/PetsApiImpl.kt
@@ -0,0 +1,16 @@
+package de.semipol.adapters.rest
+
+class PetsApiImpl: PetsApi {
+
+    override suspend fun createPets(pet: Pet) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun listPets(limit: Int?): List<Pet> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun showPetById(petId: String): Pet {
+        TODO("Not yet implemented")
+    }
+}
